7e801093ca1e6523213eeab9cea79eb273656064
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-11-18 00:54:30 +0700

    Update CrackStego.py

--- CrackStego.py
+++ CrackStego.py
@@ -35,15 +35,18 @@ from datetime import datetime
 print(f"""
 Selamat datang di CrackStego (http:/github.com/fixploit03/CrackStego/)
 ----------------------------------------------------------------------
-
+CrackStego adalah program Python yang dirancang untuk meng-crack 
+file stego yang dihasilkan oleh alat Steghide dengan menggunakan 
+teknik serangan Dictionary Attack.
 
 Peringatan
 ----------
-Program ini dirancang hanya untuk tujuan pendidikan dan penelitian yang sah.
-Dilarang keras menggunakan program ini untuk kegiatan ilegal, merusak,
-atau tanpa izin pemilik file. Pengguna bertanggung jawab penuh atas segala
-konsekuensi hukum yang mungkin timbul dari penggunaan program ini. Pastikan
-untuk selalu mematuhi peraturan dan etika yang berlaku di wilayah Anda.
+Program ini dirancang hanya untuk tujuan pendidikan dan penelitian 
+yang sah. Dilarang keras menggunakan program ini untuk kegiatan ilegal,
+merusak, atau tanpa izin pemilik file. Pengguna bertanggung jawab penuh 
+atas segala konsekuensi hukum yang mungkin timbul dari penggunaan 
+program ini. Pastikan untuk selalu mematuhi peraturan dan etika yang 
+berlaku di wilayah Anda.
 """)
 time.sleep(3)
 
